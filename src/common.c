d9ab6b82652f2743d716851cac533fb7a77bd73f
Justin Loew <jloloew@users.noreply.github.com>
2015-04-25 00:28:54 -0500

    asdf

--- src/common.c
+++ src/common.c
@@ -6,6 +6,33 @@
 const int kPebbleScreenWidth	= 144;
 const int kPebbleScreenHeight	= 152;
 
+static void coords_from_index(const int rowSizeBits, const int index, int *x, int *y) {
+	
+}
+static int index_from_coords(int rowSizeBits, int x, int y) {
+	return (rowSizeBits * y) + x;
+}
+
+BitmapLayer *bitmap_layer_rotate_right(BitmapLayer *bitmapLayer) {
+	CGFrame frame = layer_get_frame(bitmap_layer_get_layer(bitmapLayer));
+	BitmapLayer *newBitmapLayer = bitmap_layer_create(frame);
+	if (!newBitmapLayer)
+		return NULL;
+	
+	GBitmap orig = bitmap_layer_get_bitmap(bitmapLayer);
+	GSize origSize = orig.bounds.size;
+	uint16_t rowSizeBytes = orig.row_size_bytes;
+	void *origData = orig.addr;
+	uint16_t bufferSize = origSize.h * rowSizeBytes;
+	// copy bitmap data
+	void *newBuffer = malloc(bufferSize);
+	if (!newBuffer)
+		return NULL;
+	memcpy(newBuffer, origData, bufferSize);
+	
+	GBitmap newBitmap = gbitmap_create_with_data(newBuffer);
+}
+
 void game_init() {
 	
 }
--- src/common.h
+++ src/common.h
@@ -4,5 +4,7 @@ extern const int kPebbleScreenWidth;
 	// compensates for the status bar.
 extern const int kPebbleScreenHeight;
 
+BitmapLayer *bitmap_layer_rotate_right(BitmapLayer *bitmapLayer);
+
 void game_init();
 void game_deinit();
